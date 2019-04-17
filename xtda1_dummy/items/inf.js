9fa00bcbe6c69ab20bd23714182db2cb01169607
山田　宇矩 <yamadtakanori@yamadasakainorinoMacBook-puro.local>
2019-04-17 23:25:55 +0900

    fix index

--- xtda1_dummy/items/inf.js
+++ xtda1_dummy/items/inf.js
@@ -1,9 +1,4 @@
 
-if ( ! canvas || ! canvas.getContext ) { return false; }
-ctx.strokeRect(20, 20, 480, 480);
-ctx.beginPath();
-ctx.moveTo(20, 20);
-ctx.lineTo(500,500);
 timerID = setInterval('room()',100)
 
 
