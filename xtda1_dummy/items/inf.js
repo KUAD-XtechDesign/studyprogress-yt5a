0a561240f61b9e16bdd151eb421772f64d185ae4
山田　宇矩 <yamadtakanori@yamadasakainorinoMacBook-puro.local>
2019-04-17 15:10:24 +0900

    fix index

--- xtda1_dummy/items/inf.js
+++ xtda1_dummy/items/inf.js
@@ -1,41 +1,15 @@
-window.addEventListener("keydown", handleKeydown);
-aaa = 20
-bbb = aaa + 40
+
 timerID = setInterval('draw()',100)
 function draw() {
 
-  var canvas = document.getElementById('canvassample');
+var canvas = document.getElementById('canvassample');
 if ( ! canvas || ! canvas.getContext ) { return false; }
 var ctx = canvas.getContext('2d');
 ctx.beginPath();
-ctx.strokeRect(aaa, 20, bbb, 80);
-}
-
-window.addEventListener("keydown", handleKeydown);
-
-function handleKeydown(event){
-  // キーコード(どのキーが押されたか)を取得
-  var keyCode = event.keyCode;
-
-  // 右
-   if (keyCode == 39) {
-  aaa += 10
-  }
-
-  // 左
-   if (keyCode == 37) {
-  aaa -= 10
-  }
-
-  // 上
-   if (keyCode == 38) {
-  up();
-  }
-
-
-  // 下
-   if (keyCode == 40) {
-  down();
-  }
-
+ctx.strokeRect(20, 20, 480, 480);
+ctx.beginPath();
+ctx.moveTo(20, 20);
+ctx.lineTo(500,500);
+//ctx.closePath();
+ctx.stroke();
 }
