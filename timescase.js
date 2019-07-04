3d1aaca398f1bbb7aac5fd445119c7baf272894c
山田　宇矩 <yamadtakanori@yamadasakainorinoMacBook-puro.local>
2019-07-05 01:08:38 +0900

    fix index

--- timescase.js
+++ timescase.js
@@ -1,32 +1,47 @@
-
 wih = window.innerHeight;
 wiw = window.innerWidth;
+pxl = wih/20;
+pxt = wih/60;
 
 document.getElementById("timescase").innerHTML = "<canvas id='canvas1'" +
 "style='position:fixed;right:0;top:0;'" +
 "height='" + wih + "'" +
-"Width='" + 400 + "'" +
+"Width='" + pxl*3 + "'" +
 "></canvas>";
 
 var canvas = document.getElementById('canvas1');
 var context = canvas.getContext('2d');
 
-if (canvas.getContext) {
+//setup
+secp = 0;
 
+timerID = setInterval('gettime()',100);
+
+function gettime()  {
+now = new Date();
+sec = now.getSeconds();
+console.log(sec)
+getpx();
+ }
+
+function getpx()  {
+secp = sec * pxt;
 var context = canvas.getContext('2d');
 
 //左から20上から40の位置に、幅50高さ100の四角形を描く
-context.fillRect(0,0,50,wih);
+context.fillRect(0,secp,pxl,pxl);
+context.fillRect(pxl,pxl,pxl,pxl);
 
 //色を指定する
-context.strokeStyle = 'rgb(00,00,255)'; //枠線の色は青
-context.fillStyle = 'rgb(255,00,00)'; //塗りつぶしの色は赤
+//context.strokeStyle = 'rgb(00,00,255)'; //枠線の色は青
+//context.fillStyle = 'rgb(255,00,00)'; //塗りつぶしの色は赤
 
-//左から200上から80の位置に、幅100高さ50の四角の枠線を描く
-//context.strokeRect(200,80,100,50);
+//delete
+context.clearRect(0,secp-1, pxl,1);
 
-//左から150上から75の位置に、半径60の半円を反時計回り（左回り）で描く
-//context.arc(150,75,60,Math.PI*1,Math.PI*2,true);
-//context.fill();
-
+//reset
+ if (sec == 0) {
+ context.clearRect(0,0,pxl*3,wih);
+ secp = 0;
+ }
 }
