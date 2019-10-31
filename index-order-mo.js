8eb16670301851f1206966a65863b545e2b84ddc
山田　宇矩 <yamadtakanori@yamadasakainorinoMacBook-puro-3146.local>
2019-10-31 16:11:06 +0900

    fixed index

--- index-order-mo.js
+++ index-order-mo.js
@@ -10,7 +10,8 @@ setmovie=[
 ["【MAD】 意識の繭 【素晴らしき日々】","sm19632817"],
 ["カオスな競馬ゲーム　その1","sm11298955"],
 ["【第14回MMD杯本選】不明な提督が着任されました","sm25569104"],
-["あのアニメにちびまる子ちゃんのナレーションを付けてみた","sm13974077"]
+["あのアニメにちびまる子ちゃんのナレーションを付けてみた","sm13974077"],
+["【幕末志士】剣豪【実況プレイ】","sm33878367"]
 ]
 
 function movies(){
@@ -22,6 +23,7 @@ function movies(){
  chatbar.setAttribute("name","chatbar");
  chatbar.setAttribute("id","chatbar");
  document.getElementById("fronts").appendChild(chatbar)
+ document.getElementById("chatbar").style.height = "200px"
 
  for (var i = 0; i < setmovie.length; i++) {
   var front = document.createElement("div");
@@ -32,8 +34,8 @@ function movies(){
 }
 function moviemenu(mo){
  document.getElementById('fronttx').innerHTML =
- '<script type="text/javascript" src="http://ext.nicovideo.jp/thumb_watch/'+mo+'?w=510&amp;h=320"></script>'+
- '<iframe allowfullscreen="allowfullscreen" allow="autoplay" frameborder="0" width="510" height="320"'+
+ '<script type="text/javascript" src="http://ext.nicovideo.jp/thumb_watch/'+mo+'?w=620&amp;h=400"></script>'+
+ '<iframe allowfullscreen="allowfullscreen" allow="autoplay" frameborder="0" width="620" height="400"'+
  'src="http://embed.nicovideo.jp/watch/'+mo+'?oldScript=1&amp;'+
  'referer=http%3A%2F%2Fcly7796.net%2Fwp%2Fsample%2Fembed-a-niconico-video-player%2F&amp;'+
  'from=0&amp;allowProgrammaticFullScreen=1" style="max-width: 100%;"></iframe>'
